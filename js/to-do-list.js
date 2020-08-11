6e7e052b9294c72dba0f619a19c90519a5bbc9bb
Scirius <68830011+Scirius@users.noreply.github.com>
2020-08-11 09:50:36 +0200

    Added JS function for click handling of all buttons.

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,4 +1,65 @@
-var editButtons = document.querySelectorAll("button.todo-edit");
+const debugOutput=true;
+
+/* wrapper for console.log to be able to globally enable/disable output */
+let conLog = (msg) => {
+  if (debugOutput) console.log (msg);
+}
+
+/* click handler for add buttons */
+let addTask = (evt) => {
+  conLog (`Add clicked... for ${evt.target.parentNode.firstElementChild.className}`);
+} 
+
+/* click handler for edit buttons */
+let editTask = (evt) => {
+  conLog (`Edit clicked... for ${evt.target.parentNode.className}`);
+}
+
+/* click handler for delete buttons */
+let deleteTask = (evt) => {
+  conLog (`Delete clicked... for ${evt.target.parentNode.className}`);
+}
+
+/* encapsulated function to add eventListening to elements 
+   (you can call it directly if you only assign it to one new element)
+*/
+let addEventByObj = (o,eventHandler, targetFunction) => {
+  o.addEventListener(eventHandler,targetFunction);
+
+}
+
+/* assign event listener to elements by class */
+let addEventsByClassname = (elementClassname,eventHandler,targetFunction) => {
+   conLog(`Assigning ${eventHandler} to all elements (of class ${elementClassname})`);
+   let targetElements = document.querySelectorAll('.' + elementClassname);
+   conLog(`Found ${targetElements.length} elements.`)
+   for (let i = 0; i < targetElements.length; i++){
+		//targetElements[i].addEventListener(eventHandler,targetFunction);
+    addEventByObj(targetElements[i],eventHandler,targetFunction)
+	}
+}
+             
+/* script initialization, called once on load */
+let initializeTodolist = () => {
+  conLog ("DOM loaded, initializing...");
+  addEventsByClassname("add-button","click",addTask);
+  addEventsByClassname("edit-button","click",editTask);
+  addEventsByClassname("delete-button","click",deleteTask);
+  
+}
+
+/* code should be executed after DOM is fully loaded */
+document.addEventListener("DOMContentLoaded", initializeTodolist);
+
+
+
+
+
+
+
+
+
+/*var editButtons = document.querySelectorAll("button.todo-edit");
 var editTodoTask = function() {
 	if (this.parentNode.firstElementChild.className==="todo-desc"){
 		alert("Making editable: "+this.parentNode.firstElementChild)
@@ -10,14 +71,16 @@ console.log("l:"+(editButtons.length));
 for (var i=0; i<editButtons.length; i++){
 		editButtons[i].onclick = editTodoTask;
 	}
+*/
 
+/*
 	//create a list element
 
 
 
 function createListElement() {
 		var li = document.createElement("li");
-
+}
 //Strikethrough
 
 function crossOut() {
@@ -30,7 +93,7 @@ li.addEventListener("click",crossOut);
 
 function deleteListItem(){
 	li.classList.add("delete")
-
+}
 //list after click
 
 function addListAfterClick(){
@@ -40,3 +103,4 @@ function addListAfterClick(){
 }
 
 enterButton.addEventListener("click",addListAfterClick);
+*/
