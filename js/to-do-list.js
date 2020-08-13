b54cc2f3cc00f5a539817238050262318362087a
Scirius <68830011+Scirius@users.noreply.github.com>
2020-08-13 17:44:26 +0200

    Add basic edit button functionality

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,17 +1,16 @@
-const debugOutput=true;
-let addTodoButton = document.getElementById('addTodoButton');
-let inputFlexContainer = document.getElementById('addTodo');
-let parentTodoElement = document.getElementsByClassName('todos-tasks')[0];
+const debugOutput = true;
+let addTodoButton = document.getElementById("addTodoButton");
+let inputFlexContainer = document.getElementById("addTodo");
+let parentTodoElement = document.getElementsByClassName("todos-tasks")[0];
 
 /* wrapper for console.log to be able to globally enable/disable output */
 let conLog = (msg) => {
-  if (debugOutput) console.log (msg);
-}
+  if (debugOutput) console.log(msg);
+};
 
 /* click handler for add buttons */
 let addTask = (e) => {
-
-  if(inputFlexContainer.value === "") {
+  if (inputFlexContainer.value === "") {
     alert("Please enter item!");
     return;
   }
@@ -19,107 +18,106 @@ let addTask = (e) => {
 
   // ##creative wrapper for each todo-item
 
-  let todosTask = document.createElement('div');
-  todosTask.className = 'todos-task'; //accessing the class from css
+  let todosTask = document.createElement("div");
+  todosTask.className = "todos-task"; //accessing the class from css
   parentTodoElement.appendChild(todosTask); //added item-div inside wrapper div
 
   // ##Input for todo list item
 
-  let textInput = document.createElement('input');
-  textInput.setAttribute('type', 'text');
-  textInput.setAttribute("readonly", true);  //setting input as read only for later editing
-  textInput.classList.add("todo-desc","todos-input-one"); //or li inside ul/ol
+  let textInput = document.createElement("input");
+  textInput.setAttribute("type", "text");
+  textInput.setAttribute("readonly", true); //setting input as read only for later editing
+  textInput.classList.add("todo-desc", "todos-input-one"); //or li inside ul/ol
   textInput.value = inputFlexContainer.value; //value input connected to created input
   todosTask.appendChild(textInput); //appending the value to item-div
 
-
   // ##adding a checkbox input
-  let checkboxInput = document.createElement('input');
-  checkboxInput.setAttribute('type', 'checkbox');
-  checkboxInput.classList.add ('checkboxInput');
+  let checkboxInput = document.createElement("input");
+  checkboxInput.setAttribute("type", "checkbox");
+  checkboxInput.classList.add("checkboxInput");
   todosTask.appendChild(checkboxInput);
-
 
   // ##Adding the editIcon
 
-  let editIcon = document.createElement('img');
+  let editIcon = document.createElement("img");
   editIcon.classList = "edit-button";
   editIcon.src = "img/edit.svg";
   todosTask.appendChild(editIcon);
-  addEventByObj(editIcon,"click",editTask);
+  addEventByObj(editIcon, "click", editTask);
 
   // Adding the delete X icon
 
-  let deleteIcon = document.createElement('img');
+  let deleteIcon = document.createElement("img");
   deleteIcon.classList = "delete-button";
   deleteIcon.src = "img/x.svg";
   todosTask.appendChild(deleteIcon);
-  addEventByObj(deleteIcon,"click",deleteTask);
-
+  addEventByObj(deleteIcon, "click", deleteTask);
 
   inputFlexContainer.value = "";
-
-
-}
+};
 let keyUp = (evt) => {
-  if(evt.key === 'Enter' || evt.keyCode === 13) {
+  if (evt.key === "Enter" || evt.keyCode === 13) {
     addTask();
     inputFlexContainer.value = "";
   }
-}
+};
 
+/* wip */
+let endEditing = (evt) => {
+  conLog(`Keyup!`);
+};
 /* click handler for edit buttons */
 let editTask = (evt) => {
-  conLog (`Edit clicked... for ${evt.target.parentNode.className}`);
-}
+  conLog(`Edit clicked... for ${evt.target.parentNode.className}`);
+  /* this is where the new code goes */
+  let taskElement = evt.target.parentNode;
+  let inputField = taskElement.getElementsByClassName("todo-desc")[0];
+  inputField.removeAttribute("readonly");
+  inputField.focus();
+
+  // keyUp();
+  // inputField.setAttribute("readonly");
+  //console.log(taskElement);
+};
 
 /* click handler for delete buttons */
 let deleteTask = (evt) => {
-  conLog (`Delete clicked... for ${evt.target.parentNode.className}`);
+  conLog(`Delete clicked... for ${evt.target.parentNode.className}`);
   let task = evt.target.parentNode.remove();
-}
-
+};
 
 /* encapsulated function to add eventListening to elements
    (you can call it directly if you only assign it to one new element)
 */
-let addEventByObj = (o,eventHandler, targetFunction) => {
-  o.addEventListener(eventHandler,targetFunction);
-
-}
+let addEventByObj = (o, eventHandler, targetFunction) => {
+  o.addEventListener(eventHandler, targetFunction);
+};
 
 /* assign event listener to elements by class */
-let addEventsByClassname = (elementClassname,eventHandler,targetFunction) => {
-   conLog(`Assigning ${eventHandler} to all elements (of class ${elementClassname})`);
-   let targetElements = document.querySelectorAll('.' + elementClassname);
-   conLog(`Found ${targetElements.length} elements.`)
-   for (let i = 0; i < targetElements.length; i++){
-		//targetElements[i].addEventListener(eventHandler,targetFunction);
-    addEventByObj(targetElements[i],eventHandler,targetFunction)
-	}
-}
+let addEventsByClassname = (elementClassname, eventHandler, targetFunction) => {
+  conLog(
+    `Assigning ${eventHandler} to all elements (of class ${elementClassname})`
+  );
+  let targetElements = document.querySelectorAll("." + elementClassname);
+  conLog(`Found ${targetElements.length} elements.`);
+  for (let i = 0; i < targetElements.length; i++) {
+    //targetElements[i].addEventListener(eventHandler,targetFunction);
+    addEventByObj(targetElements[i], eventHandler, targetFunction);
+  }
+};
 
 /* script initialization, called once on load */
 let initializeTodolist = () => {
-  conLog ("DOM loaded, initializing...");
-  addEventsByClassname("add-button","click",addTask);
-  addEventsByClassname("edit-button","click",editTask);
-  addEventsByClassname("delete-button","click",deleteTask);
-
-}
+  conLog("DOM loaded, initializing...");
+  addEventsByClassname("add-button", "click", addTask);
+  addEventsByClassname("edit-button", "click", editTask);
+  addEventsByClassname("delete-button", "click", deleteTask);
+  /* wip */
+  /*addEventsByClassname("todo-desc", "keyup", endEditing);*/
+};
 
 /* code should be executed after DOM is fully loaded */
 document.addEventListener("DOMContentLoaded", initializeTodolist);
-
-
-
-
-
-
-
-
-
-
 
 /*var editButtons = document.querySelectorAll("button.todo-edit");
 var editTodoTask = function() {
@@ -167,20 +165,24 @@ function addListAfterClick(){
 enterButton.addEventListener("click",addListAfterClick);
 */
 
+var checkboxInput = document.getElementsByClassName("checkboxInput");
+document.addEventListener("click", function (e) {
+  if (event.target.matches(".checkboxInput")) {
+    if (e.target.checked === true) {
+      e.target.parentNode.firstElementChild.classList = "text-strike-through";
+    } else {
+      e.target.parentNode.firstElementChild.classList.remove(
+        "text-strike-through"
+      );
+      e.target.parentNode.firstElementChild.classList = "todo-desc";
+    }
+  }
+});
 
+document.addEventListener("change", (event) => {
+  if (event.target.matches(".todo-desc")) {
+    event.target.removeAttribute("readonly");
+  }
+});
 
-var checkboxInput = document.getElementsByClassName('checkboxInput');
-document.addEventListener('click', function(e){
-if(event.target.matches ('.checkboxInput')) {
-  if(e.target.checked === true){
-    e.target.parentNode.firstElementChild.classList= "text-strike-through";
-  } else {
-    e.target.parentNode.firstElementChild.classList.remove("text-strike-through");
-    e.target.parentNode.firstElementChild.classList = "todo-desc";
-   }
- }
-
-})
-
-
-inputFlexContainer.addEventListener('keyup', keyUp);
+document.addEventListener("keyup", keyUp);
